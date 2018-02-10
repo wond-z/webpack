170ae997bb7bf06e3637b2fb891cbd45a1c580fb
wond-z <wond-z@live.com>
2018-02-10 15:32:56 +0800

    资源管理demo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '你个傻逼'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。（样式文件中也含有背景图片的使用）
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
