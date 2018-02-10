9ea45d606878f70606148b7363cd211a8a0a6d66
wond-z <wond-z@live.com>
2018-02-10 17:16:34 +0800

    设定 HtmlWebpackPlugin，清理 /dist 文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   // entry: './src/index.js',
@@ -6,6 +8,12 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
     // filename: 'bundle.js',
     filename: '[name].bundle.js',
